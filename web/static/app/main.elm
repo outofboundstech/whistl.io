35e6dd4c743d9590de94c09601b3732388ad7272
Marcel Oomens <mail@marceloomens.com>
2016-02-29 19:48:58 +0100

    Attempt FileReader bindings
    Signed-off-by: Marcel Oomens <mail@marceloomens.com>

--- web/static/app/main.elm
+++ web/static/app/main.elm
@@ -1,21 +1,36 @@
-import Html exposing (div, button, text)
-import Html.Events exposing (onClick)
+import Html exposing (Attribute, div, label, input, text, p)
+import Html.Attributes exposing (id, name, class, for, type')
+import Html.Events exposing (on, onClick)
+import Json.Decode as Json exposing (Decoder, at, string)
 import StartApp.Simple as StartApp
+
+import Debug exposing (log)
 
 main = StartApp.start { model = model, view = view, update = update }
 
-model = 0
+model = "Hello, World!"
 
 view address model =
-  div []
-    [ button [ onClick address Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick address Increment ] [ text "+" ]
+  div [ class "form-group" ]
+    [ label [ class "control-label", for "upload_file" ] [ text "Documents" ]
+    , input [ type' "file", class "form-control", id "upload_file", name "upload[file]",
+        onChange address Encrypt ] [ ]
+    , p [ ] [ text model ]
     ]
 
-type Action = Increment | Decrement
+type Action
+  = Noop
+  | Encrypt String
 
 update action model =
   case action of
-    Increment -> model + 1
-    Decrement -> model - 1
+    Noop -> model
+    Encrypt filepath -> filepath
+
+-- Custom event handler
+targetFile : Json.Decoder String
+targetFile = at ["target", "files", "[0]"] string
+
+onChange : Signal.Address a -> (String -> a) -> Attribute
+onChange address contentToValue = on "change" targetFile
+  (\str -> Signal.message address (contentToValue str))
